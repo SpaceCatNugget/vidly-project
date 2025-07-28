138f0d9763b74073cbf5f25b92a656e905067f0a
SpaceCatNugget <81484715+SpaceCatNugget@users.noreply.github.com>
2025-07-28 15:57:56 +0300

    updated movie.model

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const { genreSchema } = require('./genre');
 
@@ -31,7 +32,7 @@ const Movie = mongoose.model('Movie', new mongoose.Schema({
 function validateMovie(movie) {
   const schema = Joi.object({
     title: Joi.string().min(5).max(50).required(),
-    genreId: Joi.objectId().max(50).required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).max(255).required(),
     dailyRentalRate: Joi.number().min(0).required()
   });
