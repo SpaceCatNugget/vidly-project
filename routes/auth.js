0e0cb5d9afe409cff7dd3911fe3a3027e2179351
SpaceCatNugget <81484715+SpaceCatNugget@users.noreply.github.com>
2025-07-29 18:49:32 +0300

    adding error login

--- routes/auth.js
+++ routes/auth.js
@@ -7,17 +7,22 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
 
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  const token = user.generateAuthToken();
-  res.send(token);
+    const token = user.generateAuthToken();
+    res.send(token);
+  } catch (ex) {
+    console.error("LOGIN ERROR:", ex.message);
+    res.status(500).send("Internal server error");
+  }
 });
 
 function validate(req) {
